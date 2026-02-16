78641160675385588227e29ebe440f4740148df3
IT24102134 <IT24102134@my.sliit.lk>
2026-02-16 13:40:36 +0530

    Add new Spring Boot backend

---
+++ src/test/java/com/easymed/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easymed.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
